aa107b349980a323a53134e12d833c5ac8d51ce6
ChenLiang <cliang2@caltech.edu>
2019-04-23 19:28:57 -0700

    bug fixed

--- gym_EV/envs/EV_env.py
+++ gym_EV/envs/EV_env.py
@@ -26,13 +26,13 @@ class EVEnv(gym.Env):
         self.alpha = 1
         self.beta =1
         self.gamma =1
-        self.signal = 0
+        self.signal = None
         self.state = None
-        self.feedback = None
+        # self.feedback = None
         self.time = 0
         self.time_interval = 0.1
     
-  def step(self, action):
+  def step(self, action): 
         # Update states according to a naive battery model
         self.time = self.time + self.time_interval
         # Check if a new EV arrives
@@ -40,12 +40,14 @@ class EVEnv(gym.Env):
             if data[i,0] > self.time - self.time_interval and data[i,0] <= self.time:
                 # Add a new active charging station
                 self.state[np.where(self.state[:,4==0])[0][0]] = 1
-        self.state[:,3] = self.state[:,3]  + action * self.time_interval
-        
+        self.state[:,3] = self.state[:,3]  + action.charging_rate * self.time_interval
+
         # Select a new tracking signal
         levels = np.linspace(0, 1000, num=100)
-        signal = choices(levels, self.feedback)
+        self.signal = choices(levels, action.feedback)
         unsatisfactory = 0
+        
+        
         for i in np.nonzero(self.state[:,4])[0]:
             # The EV is overdue
             if self.time - self.state[i,1] >= 0:
@@ -55,38 +57,27 @@ class EVEnv(gym.Env):
             else:
                 penalty = gamma*state[0,3]/(self.state[i,1]-self.time)
         unsatisfactory = unsatisfactory + penalty
-        reward = alpha*stats.entropy(feedback)-beta*(np.sum(action)-self.signal)**2+unsatisfactory
+        reward = alpha*stats.entropy(action.feedback)-beta*(np.sum(action.charging_rate)-self.signal)**2+unsatisfactory
         return reward
       
       
   def reset(self):
-        self.signal = []
-        # Maybe use randomly selected probability vector
-        self.feedback = np.ones(100)/100
         # Select a random day and restart
         day = random.randint(1, 65)
-        name = 'data/data' + str(day)
+        name = 'data/data' + str(day) + '.npy'
         # Load data
         data = np.load(name)
         # Initialize states and time
-        self.state = np.zeros(54,5)
+        self.state = np.zeros([54,5])
         # Arrival and Depature
         self.state[0,0:2]= data[0,0:2]
         # SOC
         self.state[0,3] = data[0,2]
         # The charging station is activated
         self.state[0,4] = 1
+        # Select initial signal to be zero -- does not matter since time interval is short
+        self.signal = 0
         self.time = data[0,0]
-        
-        
-#   Use only we have a GUI      
-#   def render(self, mode='human'):
-#     ...
 
-#   def close(self):
-#         if self.time == 24:
-#             return 1
-#         else:
-#             return 0
-   
-    ...
+        
+        
