d6d66086bf7a5fa60ee50db559b8f43f551f3610
ChenLiang <cliang2@caltech.edu>
2019-04-27 16:40:08 -0700

    add action space

--- gym_EV/envs/EV_env.py
+++ gym_EV/envs/EV_env.py
@@ -17,19 +17,6 @@ import random  # Handling random number generation
 from random import choices
 from collections import deque  # Ordered collection with ends
 
-class Action:
-
-  def __init__(self, charging_rate = None, feedback = None):
-    self.charging_rate = charging_rate
-    self.feedback = feedback
-  def set_value(self, charging_rate, feedback):
-    self.charging_rate = charging_rate
-    self.feedback = feedback
-
-  def random_sample(self):
-    self.charging_rate = np.random.rand(54)
-    self.feedback = np.random.rand(100)
-
 
 
 class EVEnv(gym.Env):
@@ -42,63 +29,91 @@ class EVEnv(gym.Env):
     self.gamma = 1
     self.signal = None
     self.state = None
-    # self.feedback = None
+
+    self.observation_space = spaces.Dict({
+      "RemainingTime": spaces.Box(low=0, high=24, shape=(54,)),
+      "RemainingEnergy": spaces.Box(low=0, high=24, shape=(54,)),
+      "IsActivated": spaces.MultiBinary(54),
+      "TrackingSignal": spaces.Box(low=0, high=1000, shape=(1,))
+    })
+
+    self.action_space = spaces.Dict({
+      "ChargingRate": spaces.Box(low=0, high=100, shape=(54,)),
+      "Feedback": spaces.Box(low=0, high=1, shape=(100,))
+    })
+
     self.time = 0
     self.time_interval = 0.1
     # store data
     self.data = None
 
   def step(self, action):
-    # Update states according to a naive battery model
+    ## Update states according to a naive battery model
+    # Time advances
     self.time = self.time + self.time_interval
     # Check if a new EV arrives
     for i in range(len(self.data)):
       if self.data[i, 0] > self.time - self.time_interval and self.data[i, 0] <= self.time:
         # Add a new active charging station
-        self.state[np.where(self.state[:, 4] == 0)[0][0]] = 1
-    self.state[:, 3] = self.state[:, 3] + action.charging_rate * self.time_interval
+        self.state[np.where(self.state[:, 2] == 0)[0][0]] = 1
 
     # Select a new tracking signal
     levels = np.linspace(0, 1000, num=100)
-    self.signal = choices(levels, action.feedback)
+    self.signal = choices(levels, action["Feedback"])
     unsatisfactory = 0
 
-    for i in np.nonzero(self.state[:, 4])[0]:
+    # Update remaining time
+    time_result = self.state[:, 0] - self.time_interval
+    self.state[:, 0] = time_result.clip(min=0)
+
+    # Update battery
+    charging_result = self.state[:, 1] - action["ChargingRate"] * self.time_interval
+    self.state[:, 1] = charging_result.clip(min=0)
+
+    for i in np.nonzero(self.state[:, 2])[0]:
       # The EV is overdue
-      if self.time - self.state[i, 1] >= 0:
+      if self.state[i, 0] == 0:
         penalty = 500 * self.gamma
         # Inactivate the EV
-        self.state[i, 4] = 0
+        self.state[i, 2] = 0
       else:
-        penalty = self.gamma * self.state[0, 3] / (self.state[i, 1] - self.time)
+        penalty = self.gamma * self.state[0, 1] / self.state[i, 0]
     unsatisfactory = unsatisfactory + penalty
-    reward = self.alpha * stats.entropy(action.feedback) - self.beta * (
-          np.sum(action.charging_rate) - self.signal) ** 2 + unsatisfactory
+    reward = self.alpha * stats.entropy(action["Feedback"]) - self.beta * (
+          np.sum(action["ChargingRate"]) - self.signal) ** 2 + unsatisfactory
 
     done = True if self.time >= 24 else False
-    obs = self.state
+    obs = {}
+    obs["state"] = self.state
+    obs["signal"] = self.signal
     info = {}
-
     return obs, reward, done, info
 
   def reset(self):
     # Select a random day and restart
-    day = random.randint(1, 65)
+    day = random.randint(1, 59)
     name = 'data/data' + str(day) + '.npy'
     # Load data
     data = np.load(name)
     self.data = data
     # Initialize states and time
-    self.state = np.zeros([54, 5])
-    # Arrival and Depature
-    self.state[0, 0:2] = data[0, 0:2]
+    self.state = np.zeros([54, 3])
+    # Remaining time
+    self.state[0, 0] = data[0, 1]
     # SOC
-    self.state[0, 3] = data[0, 2]
+    self.state[0, 1] = data[0, 2]
     # The charging station is activated
-    self.state[0, 4] = 1
+    self.state[0, 2] = 1
     # Select initial signal to be zero -- does not matter since time interval is short
     self.signal = 0
     self.time = data[0, 0]
+    obs = {}
+    obs["state"] = self.state
+    obs["signal"] = self.signal
+    return obs
+
+
+
 
 if __name__ == '__main__':
   env = EVEnv()
