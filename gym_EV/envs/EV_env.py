5a21ad00e6ca43820446790a0da81a60dfa9cd32
Tongxin <tongxin@caltech.edu>
2019-05-30 22:51:51 -0700

    fix

--- gym_EV/envs/EV_env.py
+++ gym_EV/envs/EV_env.py
@@ -18,32 +18,30 @@ from random import choices
 from collections import deque  # Ordered collection with ends
 
 
-
 class EVEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
-  def __init__(self):
+  def __init__(self, n_EVs=54, n_levels=10, max_energy=20):
     # Parameter for reward function
-    self.alpha = 1
+    self.alpha = 0
     self.beta = 5
     self.gamma = 1
     self.signal = None
     self.state = None
-    self.n_EVs = 3
-    self.n_levels = 6
-    self.max_control = 6
-    self.max_rate = 6
+    self.n_EVs = n_EVs
+    self.n_levels = n_levels
     self._max_episode_steps = 100000
     self.flexibility = 0
     self.penalty = 0
     self.tracking_error = 0
-    self.avg_tracking_error = 0
+    self.max_energy = max_energy
+    self.max_rate = 6
 
     # Specify the observation space
     lower_bound = np.array([0])
-    upper_bound = np.array([24,70])
+    upper_bound = np.array([24, 70])
     low = np.append(np.tile(lower_bound, self.n_EVs * 2), lower_bound)
-    high = np.append(np.tile(upper_bound, self.n_EVs), np.array([self.max_control]))
+    high = np.append(np.tile(upper_bound, self.n_EVs), np.array([self.max_energy]))
     self.observation_space = spaces.Box(low=low, high=high, dtype=np.float32)
 
     # Specify the action space
@@ -67,12 +65,12 @@ class EVEnv(gym.Env):
       if self.data[i, 0] > self.time - self.time_interval and self.data[i, 0] <= self.time:
         # Reject if all spots are full
         if np.where(self.state[:, 2] == 0)[0].size == 0:
-            continue
+          continue
         # Add a new active charging station
         else:
-            self.state[np.where(self.state[:, 2] == 0)[0][0], 0] = self.data[i, 1]
-            self.state[np.where(self.state[:, 2] == 0)[0][0], 1] = self.data[i, 2]
-            self.state[np.where(self.state[:, 2] == 0)[0][0], 2] = 1
+          self.state[np.where(self.state[:, 2] == 0)[0][0], 0] = self.data[i, 1]
+          self.state[np.where(self.state[:, 2] == 0)[0][0], 1] = self.data[i, 2]
+          self.state[np.where(self.state[:, 2] == 0)[0][0], 2] = 1
 
     # Update remaining time
     time_result = self.state[:, 0] - self.time_interval
@@ -80,10 +78,10 @@ class EVEnv(gym.Env):
 
     # Update battery
     charging_result = self.state[:, 1] - action[:self.n_EVs] * self.time_interval
-    # Battery is full/no EV
+    # Battery is full
     for item in range(len(charging_result)):
-        if charging_result[item] < 0:
-            action[item] = self.state[item, 1]/self.time_interval
+      if charging_result[item] < 0:
+        action[item] = self.state[item, 1] / self.time_interval
     self.state[:, 1] = charging_result.clip(min=0)
 
     self.penalty = 0
@@ -103,31 +101,36 @@ class EVEnv(gym.Env):
     # Update rewards
     # Set entropy zero if feedback is allzero
     if not np.any(action[-self.n_levels:]):
-        self.flexibility = 0
+      self.flexibility = 0
     else:
-        self.flexibility = self.alpha * (stats.entropy(action[-self.n_levels:])) ** 2
+      self.flexibility = self.alpha * (stats.entropy(action[-self.n_levels:])) ** 2
 
     self.tracking_error = self.beta * (np.sum(action[:self.n_EVs]) - self.signal) ** 2
-    self.avg_tracking_error = self.tracking_error/200 + self.avg_tracking_error
     reward = (self.flexibility - self.tracking_error - self.penalty) / 100
 
     # Select a new tracking signal
-    levels = np.linspace(0, self.max_control, num=self.n_levels)
+    levels = np.linspace(0, self.max_energy, num=self.n_levels)
     # Set signal zero if feedback is allzero
     if not np.any(action[-self.n_levels:]):
-        self.signal = 0
+      self.signal = 0
+
     else:
-        self.signal = choices(levels, weights=action[-self.n_levels:])[0]
+      self.signal = choices(levels, weights=action[-self.n_levels:])[0]
 
     done = True if self.time >= 24 else False
     obs = np.append(self.state[:, 0:2].flatten(), self.signal)
     info = {}
-    return obs, reward, done, info
+    refined_act = action
+    return obs, reward, done, info, refined_act
 
-  def reset(self):
+  def reset(self, isTrain):
     # Select a random day and restart
-    day = random.randint(1, 99)
-    name = '/Users/tonytiny/Documents/Github/gym-EV_data/real_train/data' + str(day) + '.npy'
+    if isTrain:
+      day = random.randint(0, 99)
+      name = '/Users/tonytiny/Documents/Github/gym-EV_data/real_train/data' + str(day) + '.npy'
+    else:
+      day = random.randint(0, 21)
+      name = '/Users/tonytiny/Documents/Github/gym-EV_data/real_test/data' + str(day) + '.npy'
     # Load data
     data = np.load(name)
     self.data = data
@@ -141,13 +144,8 @@ class EVEnv(gym.Env):
     self.state[0, 2] = 1
     # Select initial signal to be zero -- does not matter since time interval is short
     self.signal = 0
+    # self.time = np.floor(data[0, 0]*10) / 10.0
     self.time = data[0, 0]
-
-    # Reset reward calculator
-    self.avg_tracking_error = 0
 
     obs = np.append(self.state[:, 0:2].flatten(), self.signal)
     return obs
-
-
-
